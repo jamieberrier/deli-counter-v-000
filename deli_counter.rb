e1ca5759ee1096003882008ff6c4b29e5474d160
jamieberrier <berrier.jamie@gmail.com>
2018-07-26 04:48:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
     counter = 0
     until counter == katz_deli.size
       katz_deli.each_with_index do |name|
-        name = "#{counter + 1}. #{name}"
+        name = "#{katz_deli.index + 1}. #{name}"
         counter += 1
       end
     end
