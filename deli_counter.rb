db4e5be3f901c9cdff980ac2c5952e92bc268f39
jamieberrier <berrier.jamie@gmail.com>
2018-07-26 04:10:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,11 @@ require 'pry'
 katz_deli = []
 
 def line(katz_deli)
-
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    
+  end
 end
 
 def take_a_number(katz_deli, name)
