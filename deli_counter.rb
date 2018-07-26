5bf47665c7b43c04a040a7061ebbbcba3c9c4e90
jamieberrier <berrier.jamie@gmail.com>
2018-07-26 04:19:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,10 +7,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{katz_deli}"
-    # katz_deli.each do |name|
-    #   puts "#{katz_deli.size}. #{name} "
-    # end
+    puts "The line is currently: #{katz_deli.join(" ")}"
   end
 end
 
