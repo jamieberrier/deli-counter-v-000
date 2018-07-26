77d36bc8d80912d9a4df52481f1fb9c544f1fa1a
jamieberrier <berrier.jamie@gmail.com>
2018-07-26 04:34:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,9 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
+    katz_deli.each do |name|
+      name << ""
+    end
     puts "The line is currently: 1. #{katz_deli.join(" #{katz_deli.size}. ")}"
   end
 end
@@ -21,5 +24,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{}."
+    katz_deli.shift
   end
 end
